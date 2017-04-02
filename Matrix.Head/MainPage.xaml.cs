37f575fcbb23819aa4d6a34fc93234690f694d5d
Jason Smith <Jason Smith>
2017-04-02 19:05:10 -0400

    Add project files.

---
+++ Matrix.Head/MainPage.xaml.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using Windows.ApplicationModel.AppService;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+ 
+namespace Matrix.Head
+{
+
+    public sealed partial class MainPage : Page
+    {
+        private AppServiceConnection LEDService { get; } = new AppServiceConnection();
+
+        private string MatrixLEDServiceName { get; } = "MatrixLEDService";
+
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            SetupAppService();
+        }
+
+        private async void SetupAppService()
+        {
+            // find the installed application(s) that expose the app service PerimeterBreachService
+            var listing = await AppServiceCatalog.FindAppServiceProvidersAsync(MatrixLEDServiceName);
+            var packageName = "";
+            // there may be cases where other applications could expose the same App Service Name, in our case
+            // we only have the one
+            if (listing.Count == 1)
+            {
+                packageName = listing[0].PackageFamilyName;
+            }
+
+            // Gets or sets the name of the app service to which you want to connect.
+            LEDService.AppServiceName = MatrixLEDServiceName;
+
+            // Gets or sets the package family name for the package that contains the endpoint for the app service.
+            LEDService.PackageFamilyName = packageName;
+            
+            //open app service connection
+            var status = await LEDService.OpenAsync();
+
+            if (status != AppServiceConnectionStatus.Success)
+            {
+                //something went wrong
+                txtStatus.Text = "Could not connect to the App Service: " + status.ToString();
+            }
+            else
+            {
+                //add handler to receive app service messages (Perimiter messages)
+                LEDService.RequestReceived += PerimeterBreachService_RequestReceived;
+            }
+        }
+
+        private async void PerimeterBreachService_RequestReceived(AppServiceConnection sender, AppServiceRequestReceivedEventArgs args)
+        {
+
+            await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, async () =>
+            {
+                // if you are doing anything awaitable, you need to get a deferral
+                var requestDeferral = args.GetDeferral();
+                var returnMessage = new ValueSet();
+                var uiMessage = "";
+                var uiMessageBrush = new SolidColorBrush(Colors.Black);
+
+                try
+                {
+                    //determine message sent from the background task (perimeter notifications)
+                    var message = args.Request.Message["Perimeter Notification"] as string;
+                    switch (message)
+                    {
+                        case "Breached":
+                            uiMessage = "Perimeter Breached!!! Alert!!!";
+                            uiMessageBrush = new SolidColorBrush(Colors.Red);
+                            break;
+                        case "Secure":
+                            uiMessage = "Perimeter Secured... Move along...";
+                            break;
+                    }
+                    returnMessage.Add("Response", "OK");
+                }
+                catch (Exception ex)
+                {
+                    returnMessage.Add("Response", "Failed: " + ex.Message);
+                }
+
+                //return OK or Failed response back to the background task
+                await args.Request.SendResponseAsync(returnMessage);
+
+
+                //let the OS know that the action is complete
+                requestDeferral.Complete();
+
+                txtStatus.Text = uiMessage;
+                txtStatus.Foreground = uiMessageBrush;
+            });
+        }
+
+        private async void btnCommand_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = sender as Button;
+            string command = "";
+            var message = new ValueSet();
+            var statusColor = new SolidColorBrush(Colors.Black);
+            switch (btn.Name)
+            {
+                case "btnLEDOn":
+                    command = "Turn LED On";
+                    break;
+                case "btnLEDOff":
+                    command = "Turn LED Off";
+                    break;
+                case "btnLEDShow":
+                    command = "LED Show";
+                    break;
+            }
+            message.Add("Request", command);
+
+            //use the app service connection to send LED Command (based on button pressed)
+            var response = await LEDService.SendMessageAsync(message);
+            var result = "";
+            //analyze response from the background task
+            if (response.Status == AppServiceResponseStatus.Success)
+            {
+                result = response.Message["Response"] as string;
+            }
+            else
+            {
+                result = "Something went wrong: " + response.Status;
+                statusColor = new SolidColorBrush(Colors.Red);
+            }
+
+            await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+            {
+                txtStatus.Text = result;
+                txtStatus.Foreground = statusColor;
+            });
+        }
+    }
+}
---
+++ Matrix.Services.LED/StartupTask.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Http;
+
+using Windows.ApplicationModel.Background;
+using Windows.ApplicationModel.AppService;
+using Windows.Devices.Gpio;
+using Windows.Foundation.Collections;
+using Windows.System.Threading;
+
+// The Background Application template is documented at http://go.microsoft.com/fwlink/?LinkID=533884&clcid=0x409
+
+namespace Matrix.Services.LED
+{
+    public sealed class StartupTask : IBackgroundTask
+    {
+        /// <summary>
+        /// BackgroundTaskDeferral prevents the task from closing prematurely (http://aka.ms/backgroundtaskdeferral)
+        /// </summary>
+        private BackgroundTaskDeferral _deferral;
+
+        /// <summary>
+        /// Endpoint connection for an app service. App services enable app-to-app communication by allowing you to provide 
+        /// services from your Universal Windows app to other Universal Windows app.
+        /// </summary>
+        private AppServiceConnection _connection;
+
+
+        private ThreadPoolTimer timer5;
+
+        private int _ledPinNumber = 23;
+        private GpioPin _ledPin;
+
+        private int _ledShowPinNumber = 5;
+        private GpioPin _ledShowPin;
+        private GpioPinValue pinValue = new GpioPinValue();
+
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            // Keep BackgroundTask alive
+            _deferral = taskInstance.GetDeferral();
+            taskInstance.Canceled += OnTaskCanceled;
+
+            // Execution triggered by another application requesting this App Service
+            // assigns an event handler to fire when a message is received from the client
+            var triggerDetails = taskInstance.TriggerDetails as AppServiceTriggerDetails;
+            _connection = triggerDetails.AppServiceConnection;
+            _connection.RequestReceived += ConnectionRequestReceived;
+
+            // Initialize the LED pin
+            GpioController controller = GpioController.GetDefault();
+            _ledPin = controller.OpenPin(_ledPinNumber);
+            _ledPin.SetDriveMode(GpioPinDriveMode.Output);
+
+            _ledShowPin = controller.OpenPin(_ledShowPinNumber);
+            _ledShowPin.SetDriveMode(GpioPinDriveMode.Output);
+        }
+
+        private async void ConnectionRequestReceived(AppServiceConnection sender, AppServiceRequestReceivedEventArgs args)
+        {
+            // if you are doing anything awaitable, you need to get a deferral
+            var requestDeferral = args.GetDeferral();
+            var returnMessage = new ValueSet();
+            try
+            {
+                //obtain and react to the command passed in by the client
+                var message = args.Request.Message["Request"] as string;
+                switch (message)
+                {
+                    case "Turn LED On":
+                        _ledPin.Write(GpioPinValue.High);
+                        returnMessage.Add("Response", "OK");
+                        await args.Request.SendResponseAsync(returnMessage);
+                        //let the OS know that the action is complete
+                        requestDeferral.Complete();
+                        break;
+                    case "Turn LED Off":
+                        _ledPin.Write(GpioPinValue.Low);
+                        returnMessage.Add("Response", "OK");
+                        await args.Request.SendResponseAsync(returnMessage);
+                        //let the OS know that the action is complete
+                        requestDeferral.Complete();
+                        break;
+                    case "LED Show":
+                        LEDShow();
+                        returnMessage.Add("Response", "OK");
+                        await args.Request.SendResponseAsync(returnMessage);
+                        break;
+                }    
+            }
+            catch (Exception ex)
+            {
+                returnMessage.Add("Response", "Failed: " + ex.Message);
+            }
+        }
+
+        private void LEDShow()
+        {
+            // Creates a periodic timer and specifies a method to call after the periodic timer is complete. 
+            // The periodic timer is complete when the timer has expired without being reactivated, and the final call to handler has finished.
+            timer5 = ThreadPoolTimer.CreatePeriodicTimer(Timer_Tick5, TimeSpan.FromMilliseconds(500));
+        }
+
+        private void Timer_Tick5(ThreadPoolTimer timer) { SwitchLED(0); }
+
+        private void SwitchLED(int index)
+        {
+            pinValue = (pinValue == GpioPinValue.High) ? GpioPinValue.Low : GpioPinValue.High;
+            _ledShowPin.Write(pinValue);
+        }
+
+
+        /// <summary>
+        /// When done with timer, it should be cancelled.
+        /// </summary>
+        /// <param name="timer"></param>
+        private void CloseTimer(ThreadPoolTimer timer)
+        {
+            if (timer != null)
+            {
+                timer.Cancel();
+            }
+        }
+
+        private void OnTaskCanceled(IBackgroundTaskInstance sender, BackgroundTaskCancellationReason reason)
+        {
+            if (_deferral != null)
+            {
+                _deferral.Complete();
+            }
+        }
+    }
+}
